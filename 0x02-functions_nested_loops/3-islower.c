1820abba3ab945b79215e1f0d3ca53c98b40d217
FaroukBinLhouita <133813472+FaroukBinLhouita@users.noreply.github.com>
2023-09-13 21:28:56 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,6 @@
 /**
  * _islower - turn it to lower case
  * @c: the charcter to check
->>>>>>> 8686b60ae6c06176ac5d1d8591be78253acec9af
  * Return: 1 if the c is lower otherwise 0
  */
 int _islower(int c)

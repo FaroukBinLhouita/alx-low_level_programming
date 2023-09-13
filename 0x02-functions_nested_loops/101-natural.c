b43fbacfc45b6c5bf7d562ad5ede746f7f8116c7
farouk <faroukhoty@gmail.com>
2023-09-13 03:09:45 +0300

    betty style

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the sum of number that divided on 5 , 3
+ * Return:0 Always
+ */
+int main(void)
+{
+int sum = 0;
+
+for (int i = 0; i < 1024; ++i)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+prinf("%d", sum);
+return (0);
+}

0cfab73697db048617f623039107f144870bbc53
farouk <faroukhoty@gmail.com>
2023-09-18 22:53:55 +0300

    files

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts - print every two charchter one
+ * @str: the string
+ * Return: void
+ */
+void puts2(char *str)
+{
+int sum = 0;
+while (*str != '\0')
+{
+sum++;
+str++;
+}
+
+for (int i = 0; i < (sum - 1); i++)
+{
+_putchar(str[i * 2]);
+}
+_putchar('\n');
+}

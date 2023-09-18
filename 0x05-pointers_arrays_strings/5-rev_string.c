bb12b28a9d288287f3a0cb302556a17c65e9bdde
farouk <faroukhoty@gmail.com>
2023-09-18 22:32:41 +0300

    files

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse the string
+ * @s: the string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+char *first = s;
+char *last = s;
+
+while (*s != '\0')
+{
+last++;
+}
+last--;
+
+while (first < last)
+{
+char temp = *first;
+*first = *last;
+*last = *first;
+
+first++;
+last--;
+}
+}

3a02848bdd82acdada94df96888e1d6817221292
farouk <faroukhoty@gmail.com>
2023-09-19 02:31:20 +0300

    file 7

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,15 +8,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int sum = 0;
+int i;
 
-while (*src != '\0')
+for (i = 0; src[i] != '\0'; i++)
 {
-*dest = *src;
-dest++;
-src++;
+dest[i] = src[i];
 }
 
-*dest = '/0';
+dest[i] = '\0';
+
 return (*dest);
 }

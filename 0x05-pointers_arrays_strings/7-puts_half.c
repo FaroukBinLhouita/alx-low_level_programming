ac2d379fcf8853b78d47e3ae69549ebc9c89582b
farouk <faroukhoty@gmail.com>
2023-09-19 01:48:44 +0300

    file 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - print the last half of the string
+ * @str: the string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int divided, i, sum = 0;
+
+while (str[sum] != '\0')
+{
+sum++;
+}
+
+if (sum / 2 == 0)
+{
+divided = sum / 2;
+for (i = 0; i < sum; i++)
+{
+_putchar(str[divided + i]);
+}
+}
+else
+{
+sum--;
+divided = sum / 2;
+for (i = 0; i < (sum + 1); i++)
+{
+_putchar(str[divided + i]);
+}
+}
+}

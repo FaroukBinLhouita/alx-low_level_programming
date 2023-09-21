76235f6ad544028217165145bc7c167c36012117
farouk <faroukhoty@gmail.com>
2023-09-21 07:22:16 +0300

    file 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - make the first string has the value of him and the two
+ * @dest: the varabile will be have the whole string
+ * @src: keep the same
+ * Return: retuen the whole string
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, firstSize = 0;
+
+while (dest[firstSize] != '\0')
+{
+firstSize++;
+}
+
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[firstSize] = src[i];
+firstSize++;
+}
+return dest;
+}

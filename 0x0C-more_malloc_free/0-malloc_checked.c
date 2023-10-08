940d3fb98805713abcdb8f2d487cc1d80a056bab
farouk <faroukhoty@gmail.com>
2023-10-08 22:43:59 +0300

    files

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocate memory
+ * @b: the size of allocated it
+ * Return: if fail return 98 otherwise void
+ */
+void *malloc_checked(unsigned int b)
+{
+void *p;
+
+*p = malloc(b);
+
+if (p == NULL)
+{
+free(p);
+exit(98);
+}
+
+return (p);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - concatinate two string in a new memory
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the lenght of the memory
+ * Return: if it fails return NULL otherwise the string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+int i, j, len1, len2;
+char *p;
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+
+if (s2 == NULL)
+{
+s2 = "";
+}
+
+for (i = 0; s1[i] != '\0'; i++)
+;
+for (j = 0; s2[j] != '\0'; j++)
+;
+
+if (n < j)
+{
+p = malloc(sizeof(char) * (i + n + 1));
+}
+else
+{
+p = malloc(sizeof(char) * (i + j + 1));
+}
+
+if (p == NULL)
+{
+free(p);
+return (NULL);
+}
+
+for (len1 = 0; s1[len1] != '\0'; len1++)
+{
+p[len1] = s1[len1];
+}
+
+for (len2 = 0; s2[len2] != '\0'; len2++)
+{
+p[len1] = s2[len2];
+}
+
+return (p);
+}

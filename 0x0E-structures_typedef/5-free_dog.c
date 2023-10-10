81b385f00b8b2ded61c1fe370057e6c6b3594995
farouk <faroukhoty@gmail.com>
2023-10-11 02:53:40 +0300

    data struct

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stdlib>
+#include <stdlib.h>
 
 /**
  * free_dog - free the dog
@@ -9,10 +9,7 @@
  */
 void free_dog(dog_t *d)
 {
-if (d)
-{
 free(d->name);
 free(d->owner);
 free(d);
-}
 }

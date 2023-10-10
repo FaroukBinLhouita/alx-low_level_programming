91be6bacc00f46f26acec65fd4c994d3271316fe
farouk <faroukhoty@gmail.com>
2023-10-10 22:10:46 +0300

    data struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - a dog basic inofrmation
+ * @name: first member
+ * @age: seconed member
+ * @owner: third member
+ * 
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};

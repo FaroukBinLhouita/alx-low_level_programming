16ec876085661c03d410de23d996ab6161b69dbb
farouk <faroukhoty@gmail.com>
2023-10-12 00:35:23 +0300

    0,1,2 files

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - take an array a function pointer and pass it by loop
+ * @size: the size of the array
+ * @action: a pointer for the printing function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+
+if (!array || !action)
+{
+    return;
+}
+
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}

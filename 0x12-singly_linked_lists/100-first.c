f892e2ee4c03865a3386891c6063ac21b77322e3
farouk <faroukhoty@gmail.com>
2023-10-19 09:25:27 +0300

    advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void _constructor(void) __attribute__((_constructor));
+/**
+ * _constructor - run before main function
+ *
+ * Return: void
+ */
+void _constructor(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+ 

024882942185befb62457bec22d7263f036bb965
farouk <faroukhoty@gmail.com>
2023-10-19 07:41:38 +0300

    push file 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_list - print the list
+ * @h: the list
+ *
+ * Return: size of the list
+ */
+size_t print_list(const list_t *h)
+{
+int len = 0;
+while (h)
+{
+printf("[%d] %s\n", strlen(h->str), h->str ? h->str : "(nil)");
+len++;
+h = h->next;
+}
+}

81210a7845f451399832d265ef6f9e034379feb6
farouk <faroukhoty@gmail.com>
2023-10-19 07:54:52 +0300

    task 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
  */
 size_t print_list(const list_t *h)
 {
-int len = 0;
+size_t len = 0;
 while (h)
 {
 printf("[%ld] %s\n", strlen(h->str), h->str ? h->str : "(nil)");
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - return the number of elements in a linked
+ * @h: the linked list
+ *
+ * Return: return the lenght of elemnts
+ */
+size_t list_len(const list_t *h)
+{
+size_t i = 0;
+
+while (h)
+{
+h = h->next;
+i++;
+}
+return (i)
+}

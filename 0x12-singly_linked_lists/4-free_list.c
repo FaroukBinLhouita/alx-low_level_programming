5f6dea2a61aec60d8e728ae070c6964ff73f9d05
farouk <faroukhoty@gmail.com>
2023-10-19 09:11:46 +0300

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_list - frees the head
+ * @head: the address
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+list_t *tmp, *nextt;
+
+if (!head)
+{
+return;
+}
+
+nextt = head;
+while (nextt)
+{
+tmp = nextt->next;
+free(nextt->str);
+free(nextt);
+nextt = tmp
+}
+}

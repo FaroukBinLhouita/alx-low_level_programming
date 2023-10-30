cc9af9bbf7f49deacc50736f4e83e373d2187336
farouk <faroukhoty@gmail.com>
2023-10-30 19:43:40 +0200

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include <main.h>
+
+/**
+ * read_textfile - reading the file
+ * @filename: the file name
+ * @letters: letters will read
+ *
+ * Retrun: 0 if success otherwise -1
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{ 
+ssize_t fd, count;
+char *str;
+
+if (!filename || !letters)
+{
+return (0);
+}
+
+str = malloc(sizeof(char) * letters);
+fd = open(filename, O_RDONLY);
+
+if (!buff || fd < 0)
+{
+return (0);
+}
+
+count = read(fd, str, letters);
+if (count < 0)
+{
+return (0);
+}
+count = write(STDOUT_FILENO, str, count);
+if (count < 0)
+return (0);
+
+free(str);
+close(fd);
+return (count);
+}

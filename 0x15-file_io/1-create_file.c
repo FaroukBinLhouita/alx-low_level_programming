554cb5118a0257aa83bde40998b5e35c34369719
farouk <faroukhoty@gmail.com>
2023-10-30 21:20:13 +0200

    task 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - returns the lenght of a string
- * @s: the string
- *
- * Return: integer
- */
-int _strlen(char *str)
-{
-int i = 0;
-if (*str++)
-i++;
-return (i);
-}
-/**
  * create_file - create a new file
  * @filename: the name of file
  * @text_content: text should write
@@ -22,7 +9,7 @@ return (i);
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-ssize_t count, len = _strlen(text_content);
+ssize_t count, len = strlen(text_content);
 
 if (!filename)
 {
@@ -30,10 +17,14 @@ return (-1);
 }
 fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (fd < 0)
+{
 return (-1);
+}
 
 if (len)
+{
 count = write(fd, text_content, len);
+}
 
 close(fd);
 return (count == len ? 1 : -1);

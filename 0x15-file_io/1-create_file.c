48bd9bf128776fe9403009a80b208513e1f486c4
farouk <faroukhoty@gmail.com>
2023-10-30 21:13:16 +0200

    task 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ return (i);
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-ssize_t count, len = strlen(text_content);
+ssize_t count, len = _strlen(text_content);
 
 if (!filename)
 {
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include <main.h>
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: the string
+ *
+ * Return: integer
+ */
+int _strlen(char *str)
+{
+int i = 0
+
+if (*str++)
+{
+i++;
+}
+return (i);
+}
+/**
+ * 
+ * 
+ * 
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+}
